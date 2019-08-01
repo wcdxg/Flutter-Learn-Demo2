1530d0767b61a3b27aa0c1d17c5bad3fad5a7132
Yuaihen <706375160@qq.com>
2019-08-01 17:39:01 +0800

    底部导航栏以及导航栏的切换

--- lib/bottom_navigation_widget.dart
+++ lib/bottom_navigation_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'pages/email.dart';
+import 'pages/home.dart';
+import 'pages/shopping.dart';
+
 void main() => runApp(BottomNavigationWidget());
 
 class BottomNavigationWidget extends StatefulWidget {
@@ -8,50 +12,56 @@ class BottomNavigationWidget extends StatefulWidget {
 }
 
 class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
-  final _BottomNavigationColor = Colors.redAccent;
+  final _bottomNavigationColor = Colors.redAccent;
+  int _currentIndex = 0;
+  List<Widget> list = List();
+
+  @override
+  void initState() {
+    list..add(Home())..add(Shopping())..add(Email());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: list[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.home,
-                color: _BottomNavigationColor,
+                color: _bottomNavigationColor,
               ),
               title: Text(
                 'Home',
-                style: TextStyle(color: _BottomNavigationColor),
+                style: TextStyle(color: _bottomNavigationColor),
               )),
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.shopping_cart,
-                color: _BottomNavigationColor,
+                color: _bottomNavigationColor,
               ),
               title: Text(
                 'Shopping',
-                style: TextStyle(color: _BottomNavigationColor),
+                style: TextStyle(color: _bottomNavigationColor),
               )),
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.gamepad,
-                color: _BottomNavigationColor,
+                color: _bottomNavigationColor,
               ),
               title: Text(
                 'Page',
                 style: TextStyle(color: _BottomNavigationColor),
               )),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.email,
-                color: _BottomNavigationColor,
-              ),
-              title: Text(
-                'Email',
-                style: TextStyle(color: _BottomNavigationColor),
-              )),
         ],
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
---
+++ lib/pages/shopping.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Shopping extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping'),),
+      body: Center(
+        child: Text('Shopping'),
+      ),
+    );
+  }
+}
