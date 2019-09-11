0ecfd9c4113aff72b26103727cefe7e518867a4a
Yuaihen <706375160@qq.com>
2019-09-11 11:41:56 +0800

    贝塞尔曲线

---
+++ lib/12.贝塞尔曲线.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '贝塞尔曲线',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ClipPath(
+            clipBehavior: Clip.antiAlias,
+            clipper: BottomClipper(),
+            child: Container(
+              color: Colors.green,
+              height: 200,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BottomClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    var path = Path();
+    //结束Y点坐标
+    var endPoint = size.height - 50;
+    path.lineTo(0, 0);
+    path.lineTo(0, endPoint);
+    //贝塞尔曲线的控制点结束点
+    var firstControlPoint = Offset(size.width / 2, size.height);
+    var firstEndPoint = Offset(size.width, endPoint);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+    //path四个点连线
+    path.lineTo(size.width, endPoint);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return false;
+  }
+}
